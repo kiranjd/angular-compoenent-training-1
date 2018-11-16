419c9ab2b5ba04340021f3cdc2d89f41bf55302e
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-11-16 18:38:46 +0200

    init commit

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  eventCount = 0;
+  ref = null;
+  @Output() gameEvent = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  startGame() {
+    this.ref = setInterval(() => {
+      this.gameEvent.emit(++this.eventCount);
+      console.log(this.eventCount);
+    }, 1000);
+  }
+
+  stopGame() {
+    clearInterval(this.ref);
+  }
+
+}
